4cc64f2821b20386460a95f9dbb911f914ac86c1
CHAHBAR-SOUFIANE <chahbar.soufiane.dev@gmail.com>
2021-06-16 12:03:53 +0100

    Bonjour, monde !

--- chapitre_1/script.js
+++ chapitre_1/script.js
@@ -1,7 +1,15 @@
-const title =React.createElement("h1",{},"Bonjour, monde !") 
-console.log(title)
-ReactDOM.render(title,document.querySelector("#id01"))
+var n = 0
+function render(){
+    const title =React.createElement("h1",{},"Bonjour, monde !",
+             React.createElement("span",{}, n))
+    ReactDOM.render(title,document.querySelector("#id01"))
+}
 
+render()
+window.setInterval(() => {
+    n++
+    render()
+},1000)
 
 
 
